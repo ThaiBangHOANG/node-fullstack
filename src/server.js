e74a70821f4f5d0384fcb110db294bee609c705a
thaibanghoang <thaibang1983hn@gmail.com>
2021-10-11 21:35:51 +0200

    s

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoute(app);
-connectDB(app);
+connectDB();
 
 let port = process.env.PORT || 8080; // use or here to make sure the app not die
 
