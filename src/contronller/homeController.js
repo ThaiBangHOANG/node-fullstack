010b4b3a62690d7ec8aeeb2a77fd12d8ac1a65ad
thaibanghoang <thaibang1983hn@gmail.com>
2021-10-15 22:34:34 +0200

    edit user

--- src/contronller/homeController.js
+++ src/contronller/homeController.js
@@ -43,10 +43,44 @@ let displayGetCRUD = async (req, res) => {
   });
 };
 
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    // check user data not found
+
+    // console.log("---------------");
+    // console.log(userData);
+    // console.log("---------------");
+    // let userData;
+    return res.render("editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("User Not Found!");
+  }
+};
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  // return res.send("Update done!");
+  return res.render("displayCRUD.ejs", {
+    dataTable: allUsers,
+  });
+};
+
+let deleteCRUD = async (req, res) => {
+  return res.send("Delete CRUD");
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getAnotherFile: getAnotherFile,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
+  deleteCRUD: deleteCRUD,
 };
